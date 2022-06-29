ae261a3e07bcd303c069fd88685a7763f0bef979
sumi.creator <sumi.creator@gmail.com>
2022-06-29 20:53:38 +0300

    content sess 17

--- sumi_dragos/sesiunea 17/sumi.js
+++ sumi_dragos/sesiunea 17/sumi.js
@@ -1,5 +1,6 @@
 
 /**
+ *
  * variabile
  * 
  * var - nu se mai foloseste
@@ -66,3 +67,66 @@ console.log(anotherInfo)
 console.log(fullName);
 console.log('My full name is ' + fullName);
 console.log("My full name is " + fullName);
+
+
+/**
+ * Boolean
+ */
+
+let lightsOn = false;
+lightsOn = true;
+
+
+/**
+ * Undefined type
+ */
+
+let dogName;
+let catName = `Pisu`;
+let dogAge = 5 ;
+console.log(dogName + catName);
+console.log(dogName + dogAge);
+dogName = `Bella`;
+
+
+/**
+ * Objects
+ */
+
+
+const person = {
+    fullName: `Sumila Dragos`,
+    age: `22`,
+    lovesCold: false,
+    nationality:`Romanian`,
+    address: {
+        city:`Tulcea`,
+        zipcode:`827050`
+    },
+    "residence-permit":true
+};
+console.log(person);
+console.log(person.fullName);
+console.log(person.address.zipcode);
+console.log(person[`nationality`]);
+console.log(person["residence-permit"]);
+
+const nat = `nationality`;
+const nationality = `American`;
+console.log(person[nat])
+
+
+/**
+ * Array
+ * 
+ * SUB TIPURI DE OBIECTE (REFERINTA)
+ * LISTE
+ * VALORILE LE PUTEM ACCESA IN FUNCTIE DE POZITIA LOR 
+ */
+
+
+const daysOffWeek = [`Monday`,`Tuesday`,`wendsday`,`4`];
+console.log(daysOffWeek);
+console.log(daysOffWeek.length);
+console.log(daysOffWeek[1]);
+console.log(daysOffWeek[daysOffWeek.length -1]);
