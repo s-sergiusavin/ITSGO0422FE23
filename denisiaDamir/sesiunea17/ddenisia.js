726e09750e962762e23655eac3c5300c03009fca
Denisia101999 <denisia.damir99410@gmail.com>
2022-07-06 18:33:23 +0300

    sesiuni

--- denisiaDamir/sesiunea17/ddenisia.js
+++ denisiaDamir/sesiunea17/ddenisia.js
@@ -1,9 +1,9 @@
-//primitives:number.string.boolean.undefind,symbol
-//references:objects:objects,array,functions,nul
+//primitives:number,string,boolean,undefind,symbol
+//references:objects:objects,array,functions,null
 
 //Numbers
 let myNumber = 5;
-let secondNumber=2;
+let secondNumber = 2;
 let result;
 
 result = myNumber + secondNumber;
@@ -15,47 +15,61 @@ result = myNumber % secondNumber;
 
 //strings 
 
-let myString = "acesta este un mesaj" + result + "2 + 2";
+let myString = "Acesta este un mesaj" + result + "2 + 2";
 myString = "O'heary";
 myString = " ";
-myString = 'acesta este un mesaj $[result]2 + 2$[3 + 3]';
+myString = `acesta este un mesaj ${result}2 + 2 ${3 + 3}`;
+
 
 //Booleans
 
-letlighsOn = false ;
+letlighsOn = false;
 
 //Arrays 
 
-const myArray = [5, 'test', true,]
+const myArray = [5, 'test', true, [3, '53'], undefined, null, {name: 'denisia'}];
+console.log(myArray[3][1]);
+console.log(myArray[6].name);
+
+
 
 // Objects
 
 const myObject = {
-    burger: "Big Mac",
-    juice:"cola",
-    'french fries' : 'with fries',
+    burger: 'Big Mac',
+    juice: 'Cola',
+    'french fries': 'with fries',
+    adress: {
+        street: 'street',
+        number: 1 ,
+    } ,
+    souce: [`ketchup`, `mayo`] ,
 }
 
 
-//Loops--bucle
+  //  Loops--bucle 
 
 let marksOf10Lei = 0;
 marksOf10Lei = marksOf10Lei + 1;
 marksOf10Lei += 1; //cea mai folosita 
-marksOf10Lei+1;
+marksOf10Lei++;
 marksOf10Lei+3; // se adauga 3 la valoarea precedenta
 ++marksOf10Lei;
 
 let total = 10;
 
+marksOf10Lei = marksOf10Lei + 1;
+marksOf10Lei = marksOf10Lei + 1;
+marksOf10Lei = marksOf10Lei + 1;
 
-// While loop
+
+// While loop-- cat timp 
 
 while (marksOf10Lei<total) {
     marksOf10Lei = marksOf10Lei + 1;
 }
 
-console.log ('Avem $[marksOf10Lei] bancnote de 10 lei');
+console.log (`Avem ${marksOf10Lei} bancnote de 10 lei`);
 
 let roomTemperature = 27;
 let desiredTemperature = 21;
@@ -63,6 +77,16 @@ let desiredTemperature = 21;
     roomTemperature -= 1;
 
  }
+
+ let acIsOn = false;
+
+while (!acIsOn) {
+// while (acIsOn !== true) {    
+    roomTemperature += 1;
+    if (roomTemperature === 35) {
+        acIsOn = true;
+    }
+}
 
  // Do while loop== nu se foloseste
 
@@ -73,11 +97,14 @@ let desiredTemperature = 21;
 
  //For loop --cea mai folosita
 
+
+ // for : initializare,conditie,actiune la finalul fiecarei iteratii
+
  let marksOf5Lei = 8;
  let totalof5Lei = 5;
 
- for (let index =0; index <totalof5Lei; index++) {
-    marksOf5Lei +=5
+ for (let index = 0; index < totalof5Lei; index++) {
+    marksOf5Lei +=1;
 
  }
 
@@ -102,7 +129,7 @@ if (var1 === var2) {
 }
 
 if (var1 !== var2) {
-    console.log('Variabilele nu  sunt egale');
+    console.log('Variabilele nu sunt egale');
 
 } else {
     console.log('Variabilele sunt egale');
@@ -130,7 +157,7 @@ if (optimalTemperature < tempperature) {
 // && daca cel putin o conditie este false,totul este fals
 //  || daca cel putin o conditie este adevarata, totul este adevarat
 
-let pizzaPrice =40;
+let pizzaPrice =  40;
 let hasGorgonzola = true;
 
 if (pizzaPrice <= 30 || hasGorgonzola === true){
@@ -178,7 +205,7 @@ removeFrom100(20);
 
 function sendEmail (firstName, lastName, proffesion, result, exam){
     if (proffesion){
-        console.log('Ai profesia ' + proffesion)
+        console.log('Ai profesia ' + proffesion);
     }
     console.log(`Salutare ${lastName} ${firstName} ,
     Ai obtinut nota ${result} la examenul  ${exam} ,
@@ -198,10 +225,10 @@ function sendEmail (firstName, lastName, proffesion, result, exam){
  function temperaturaAfara (temperature){
     console.log('Temperatura exterioara este ' + temperature)
  }
- temperaturaAfara(40)
+ temperaturaAfara(40);
 
 function orderPizza(i1, i2, i3){
-    console.log('As dori sa comand o pizza cu: ' + ( i1 + i2 + i3));
+    console.log(`As dori sa comand o pizza cu: ${i1},${i2}, ${i3}`);
 }
 
 orderPizza('salami','ruccola','cherry');
@@ -209,16 +236,20 @@ orderPizza("pastrama", "ciuperci", "masline");
 
 function doSum (a, b) {
   console.log('Suma este:' + (a + b));
+  return a + b;
 }
  //doSum(5,7);
 
- let sumResult = doSum (a,b);
+ let sumResult = doSum (5,7);
  console.log(sumResult)
 
-
- function doAnotherPizza (i1, i2, i3){
-    console.log ('Pizza cu' + i1 + i2 + i3 + "in preparare");
-    pizzaOrder
+ let pizzaOrdered = 0;
+ function doAnotherPizza(i1, i2, i3){
+    console.log ('Pizza cu ' + i1 +" " + i2 + ' '+ i3 + ' '+ "in preparare");
+    pizzaOrdered += 1;
+    return pizzaOrdered;
  }
- const anounce = doAnotherPizza ("ciuperci", "ardei", "prosciutto");
- console.log(anounce)
+ let anounce = doAnotherPizza ("ciuperci", "ardei", "prosciutto");
+  anounce = doAnotherPizza ("ciuperci", "ardei", "prosciutto");
+  anounce = doAnotherPizza ("ciuperci", "ardei", "prosciutto");
+ console.log(anounce);
