0b065df1295cbc0254439fd6653aac89928f9f98
lucianoltean <lucianoltean@gmail.com>
2022-07-11 20:59:27 +0300

    Session 19 Final

--- loltean/session 19/loltean19.js
+++ loltean/session 19/loltean19.js
@@ -94,3 +94,172 @@ const verifyNumberArrow = () => {
 logStuff(verifyNumber(80));
 logStuff(verifyNumber(30));
 
+const puppy = {
+    name: `Rex`,
+    age: 1,
+    favouriteToys: [`Duck`, `Cat`, `Bone`],
+    bark: function () {
+        console.log(`Ham!`);
+    },
+    barkLoud: () => {
+        console.log(`HAM`)
+    }
+}
+
+const anotherPuppy = puppy;
+const yetAnotherPuppy = {
+    name: `Rexx`,
+    bark() {
+        console.log(`Ham`);
+    },
+    age: 2
+}
+
+anotherPuppy.bark();
+console.log(anotherPuppy.name);
+anotherPuppy.name = `Rexxx`;
+console.log(anotherPuppy.name);
+console.log(puppy.name);
+
+function nameDog(name) {
+    return `Dog ${name}`;
+}
+
+const rex = nameDog(`Rex`);
+console.log(rex);
+
+const rexx = nameDog;
+// const rexx = () => {
+//     return `Dog ${name}`;
+// }
+console.log(rexx(`Rexxx`));
+
+const menu = {
+    burger: `Big Mac`,
+    juice: `Cola`,
+    size: `Big`,
+    'french-fries': `With salt`
+}
+
+console.log(menu.burger);
+console.log(menu["french-fries"]);
+
+const createString = (array) => {
+    let newString = ``;
+    for (let i = 0; i < array.length; i ++) {
+        newString += array[i];
+    }
+    return newString;
+}
+
+console.log(createString([3,"5", 7, 2, 1]));
+console.log(createString([2,"3425", 17, 22, 1]));
+
+const reverseValue = array => { //tot arrow function, insa parantezele pot lipsi cand avem un singur parametru
+    let newString = ``;
+    for (let i = array.length - 1; i >= 0; i --) {
+        newString += array[i];
+    }
+    return newString;
+};
+
+console.log(reverseValue([1,2,3,4]));
+
+const reverseArray = (array) => { //tot arrow function, insa parantezele pot lipsi cand avem un singur parametru
+    let newArray = [];
+    for (let i = array.length - 1; i >= 0; i --) {
+        newArray.push(array[i]);
+    }
+    return newArray;
+};
+
+console.log(reverseArray([1,2,3,4]));
+
+const createArray = number => {
+    let newArray = [];
+    for (let i = 0; i <= number; i++) {
+        newArray.push(i)
+    }
+    return newArray;
+}
+
+console.log(createArray(8));
+
+// Smart home app function
+
+let lightsValue = false;
+let acValue = false;
+let curtainsValue = true;
+let windowsValue = false;
+
+const toggleLights = () => {
+    lightsValue = !lightsValue;
+    if (lightsValue === true);
+    if (lightsValue) {
+        return `Luminile au fost aprinse`;
+    } else {
+        return `Luminile au fost stinse`;
+    }
+}
+
+
+const toggleAc = () => {
+    acValue = !acValue;
+    if (acValue) {
+        return `AC-ul a fost pornit`;
+    } else {
+        return `AC-ul a fost oprit`;
+    }
+};
+
+const toggleCurtains = () => {
+    curtainsValue = !curtainsValue;
+    if (curtainsValue) {
+        return `Draperiile au fost deschise`;
+    } else {
+        return `Draperiile au fost inchise`;
+    }
+};
+
+const toggleWindows = () => {
+    windowsValue = !windowsValue;
+    if (windowsValue) {
+        return `Geamurile au fost deschise`;
+    } else {
+        return `Geamurile au fost inchise`;
+    }
+};
+
+const smoke = () => {
+
+    if (!windowsValue) {
+        toggleWindows();
+    }
+
+    if (acValue) {
+        toggleAc();
+    }
+
+    console.log(`Smoking...`);
+
+    if (`Winter`) {
+        toggleWindows();
+    }
+
+}
+
+const playMusic = (song = `Celine Dion`) => {
+    console.log(`Playing .. ${song}`);
+}
+
+
+
+
+
+
+toggleLights();
+console.log(toggleLights());
+toggleAc();
+toggleCurtains();
+toggleWindows();
+smoke();
