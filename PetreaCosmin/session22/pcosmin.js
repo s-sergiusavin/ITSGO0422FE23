617795139cb9310fa0b1cecb76f97528580d7741
cosminpetrea33 <cosminpetrea33@yahoo.com>
2022-07-26 00:46:38 +0300

    newCommit

--- PetreaCosmin/session22/pcosmin.js
+++ PetreaCosmin/session22/pcosmin.js
@@ -27,7 +27,7 @@ console.dir(newImageElement);
 //Event listeners
 
 const unlistElement = document.getElementsByTagName("ul")[0];
-const listItemsElements = document.querySelectorAll("li");
+let listItemsElements = document.querySelectorAll("li");
 
 // unlistElement.addEventListener("click", () => {
 //   unlistElement.classList.toggle("blue");
@@ -36,20 +36,69 @@ const listItemsElements = document.querySelectorAll("li");
 for (let i = 0; i < listItemsElements.length; i++) {
   const listElement = listItemsElements[i];
   console.log(listElement);
-//   listElement.addEventListener("mouseover", function () {
-//     this.classList.add("red");
-//   });
-//   listElement.addEventListener("mouseout", function () {
-//     this.classList.remove("red");
-//   });
+  //   listElement.addEventListener("mouseover", function () {
+  //     this.classList.add("red");
+  //   });
+  //   listElement.addEventListener("mouseout", function () {
+  //     this.classList.remove("red");
+  //   });
   listElement.addEventListener("click", toggleListItems);
 }
 function toggleListItems() {
   const toggleValue = this.classList.toggle("red");
   console.log(toggleValue);
-  if(toggleValue){
-
-  }else{
-    
+  if (toggleValue) {
+    const span = document.createElement("span");
+    span.innerHTML = "x";
+    this.appendChild(span);
+  } else {
+    this.getElementsByTagName("span")[0].remove();
+    // this.remove();
   }
+}
+
+const clickMeElement = document.getElementById("clickMe");
+clickMeElement.style.cursor = "pointer";
+
+const demoInputElement = document.getElementById("demoInput");
+demoInputElement.addEventListener("keypress", function (event) {
+  console.log(event);
+  if (event.keyCode === 13 && demoInputElement.value.length > 3) {
+    demoInputElement.style.background = "green";
+    console.log(demoInputElement.value);
+    console.log(demoInputElement.value.length);
+  }
+});
+
+clickMeElement.addEventListener("click", function () {
+  const newListElement = document.createElement("li");
+  unlistElement.appendChild(newListElement);
+  listItemsElements = document.querySelectorAll("li");
+  if (demoInputElement.value) {
+    newListElement.innerHTML = demoInputElement.value;
+  } else {
+    newListElement.innerHTML = "Elem " + listItemsElements.length;
+  }
+});
+
+const toDoInputElement = document.getElementById("toDoInput");
+const addButtonElement = document.getElementsByClassName("addButton")[0];
+const toDoListElement = document.getElementsByClassName("toDoListItems")[0];
+
+addButtonElement.addEventListener("click", function () {
+  if (toDoInputElement.value) {
+    const newListItem = document.createElement("li");
+    newListItem.className = "newListItems";
+    newListItem.addEventListener("dblclick", function () {
+      this.remove();
+    });
+    toDoListElement.appendChild(newListItem);
+    newListItem.innerHTML = toDoInputElement.value;
+    toDoInputElement.value = "";
+  }
+});
+
+function checkPlan() {
+  const newListItemsNumber = document.getElementsByClassName("newListItems");
+  return newListItemsNumber < 3;
 }
