f68c567aa6bdcb5948ea1239db45a3685beaf047
adrianlazarpop <adrian.lazar.pop@gmail.com>
2022-07-27 21:04:22 +0300

    Session 23

--- adrianPop/Session22/adrian22.js
+++ adrianPop/Session22/adrian22.js
@@ -133,3 +133,136 @@ function checkPlan() {
 /**
  * Regexp
  */
+
+let string = 'zzzabcdef';
+let pattern = /ab/;
+
+console.log(pattern.exec(string));
+console.log(pattern.test(string));
+console.log(string.match(pattern));
+
+const submitBtn = document.getElementById('login');
+const emailField = document.querySelector('#email');
+const passwordField = document.querySelector('#password');
+const error = document.querySelector('#error');
+
+const validatePassword = (value) => {
+    // if (value === 'password') {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+    return value === 'password';
+}
+
+const validateEmail = (value, regex) => {
+
+    return !!value.match(regex); // !! forteaza o valoare sa fie booleana
+    // const testt = 'test';
+    // console.log(testt);
+    // console.log(!testt);
+    // console.log(!!testt);
+}
+
+submitBtn.addEventListener('click', function (event) {
+    event.preventDefault(); // anuleaza default-ul
+    // console.log(event);
+    const emailValue = emailField.value;
+    const passwordValue = passwordField.value;
+    const regexEmailPattern = /\D{4,}\@\D{4,}\.\D{2,}/g;
+
+    if (emailValue === '' || passwordValue === '') {
+        error.style.display = 'block';
+        error.innerHTML = 'All fields are rquired and must have a value';
+        error.style.color = 'red';
+    } else {
+        //Validate password
+        //Validate email
+
+        if (validatePassword(passwordValue) && validateEmail(emailValue, regexEmailPattern)) {
+            error.style.display = 'none';
+            alert('Logged in');
+            emailField.value = '';
+            passwordField.value = '';
+        } else {
+            alert('Try again');
+            error.style.display = 'block';
+            error.style.color = 'red';
+            error.innerHTML = 'Incorrect email or password';
+            emailField.value = '';
+            passwordField.value = '';
+        }
+    }
+});
+
+let variabila = 'asd';
+
+function testScoping(variabila) {
+    variabila = 'ceva';
+    console.log(variabila);
+}
+
+
+testScoping(variabila); // ceva
+console.log(variabila); // asd
+
+
+const firstNameField = document.getElementById('firstName');
+const lastNameField = document.getElementById('lastName');
+const ageField = document.getElementById('age');
+
+const firstName = 'Angelina';
+const lastName = 'Jolie';
+const age = 27;
+
+// firstNameField.value = firstName;
+// lastNameField.value = lastName;
+// ageField.value = age;
+
+
+/**
+ * Built in fuctions
+ */
+// setTimeout executa o functie pe care o primeste ca prim parametru, o singura data, dupa o anumita perioada de dimp
+// setTimeout(() => {
+//     firstNameField.value = firstName;
+//     lastNameField.value = lastName;
+//     ageField.value = age;
+// },2000); 
+// se executa dupa ce se incarca tot codul + o periada de timp daca dau si al 2-lea parametru.
+
+function setfields() {
+    firstNameField.value = firstName;
+    lastNameField.value = lastName;
+    document.getElementById('age').value = age;
+}
+
+// setTimeout(setfields, 2000); // dupa 2 secunde apeleaza referinta!!!!
+
+function myFunction() {
+    return 'Rolul parantezelor';// return implicit undefined daca nu e pus
+}
+const myFunction2 = () => {
+    return 'Rolul parantezelor'
+}
+
+const newFunction = myFunction();
+const anotherFunction = myFunction;
+
+console.log(newFunction);
+console.log(anotherFunction);
+
+/**
+ * setInterval
+ * apeleaza o functie de mai multe ori cu frecventa intervalului mentionat
+ */
+
+let start = 1;
+const interval = setInterval(() => {
+    console.log(start);
+    start += 1;
+    if (start === 7) {
+        setfields();
+        clearInterval(interval);
+    }
+}, 1000);
